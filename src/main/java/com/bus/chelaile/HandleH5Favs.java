bb41f6e52ac84b29829dbd3cf7bb9a616ac799bc
qiekk <daiding106@126.com>
2018-03-20 16:04:09 +0800

    1

--- src/main/java/com/bus/chelaile/HandleH5Favs.java
+++ src/main/java/com/bus/chelaile/HandleH5Favs.java
@@ -19,20 +19,29 @@ public class HandleH5Favs {
 	static final String URLPOST = "https://api.weixin.qq.com/cgi-bin/user/info/batchget?access_token=%s";
 	static final String URLGET = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=%s&openid=%s&lang=zh_CN";
 
-	static ICache client = new OCSCacheUtil("121.40.210.76", "7078", "9c0e27d0f09544c9", "Yuanguang2014");
+	static ICache client1 = new OCSCacheUtil("d960fa96e23843f0.m.cnhzaliqshpub001.ocs.aliyuncs.com", "11211",
+			"d960fa96e23843f0", "Chelaileocs2015");
+	static ICache client2 = new OCSCacheUtil("9c0e27d0f09544c9.m.cnhzaliqshpub001.ocs.aliyuncs.com", "11211",
+			"9c0e27d0f09544c9", "Yuanguang2014");
 	static final Set<String> OPENIDSET = new HashSet<String>(); // all openids
 	static final Set<String> lines = new HashSet<String>(); // all records
-	static final Map<String, String> OPENID_UNIONID = new HashMap<String, String>(); // openId-->unionId#type		//type: 1 公众号， 2 小程序
-	static final Set<String> KEYS = new HashSet<String>();  // 去重用 , unionId+cityId+lineId+stopName+nextStopName
-	
+	static final Map<String, String> OPENID_UNIONID = new HashMap<String, String>(); // openId-->unionId#type
+																						// //type:
+																						// 1
+																						// 公众号，
+																						// 2
+																						// 小程序
+	static final Set<String> KEYS = new HashSet<String>(); // 去重用 ,
+															// unionId+cityId+lineId+stopName+nextStopName
+
 	static BufferedReader reader;
 	static BufferedWriter writer;
-	static String fileIn = "D:/favFile.temp";
-	static String fileOut = "D:/favout.txt";
+	static String fileIn = "/data/quekunkun/favzhuanyi/favFile.txt";
+	static String fileOut = "/data/quekunkun/favzhuanyi/favout.txt";
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("1111111111111");
-//		handleFavFile();
+		handleFavFile();
 
 		System.exit(1);
 	}
@@ -51,13 +60,6 @@ public class HandleH5Favs {
 		while ((buf = reader.readLine()) != null) {
 			totalN++;
 			String bufS[] = buf.split("#");
-			// TODO test length
-			// if(bufS.length != RECORDSIZE) {
-			// System.out.println("record's length is not right, length=" +
-			// bufS.length);
-			// continue;
-			// notHandleN ++;
-			// }
 			lines.add(buf);
 			OPENIDSET.add(bufS[0]);
 		}
@@ -90,8 +92,6 @@ public class HandleH5Favs {
 		System.out.println("notHandle=" + notHandleN);
 	}
 
-	
-	
 	/**
 	 * @param openidunion2
 	 * @param openidset2
@@ -99,37 +99,32 @@ public class HandleH5Favs {
 	private static void getOpenIdUnionId() {
 		// openidunion2.put("ozKKGuBw5F3Vs6XpUza9LEAH3bJc", "1231231231321213");
 		for (String openId : OPENIDSET) {
-//			System.out.println(client.get("WECHATSIGNATUREACCESSTOKEN"));
-//			String url = String.format(URL, client.get("WECHATSIGNATUREACCESSTOKEN"));
-			
+			// System.out.println(client.get("WECHATSIGNATUREACCESSTOKEN"));
+			// String url = String.format(URL,
+			// client.get("WECHATSIGNATUREACCESSTOKEN"));
+
 			int type = 1;
-			String w1Token = "";
-			String url = String
-					.format(URLGET,
-							"7_74kUZNykGaruXlOxMUEIznavaLl0l1H7M_j_SI5s8gCLU3SwPWq-zNI7_6wUYArjd51koIpci3p_MX5tloGOkSW5Z35Ff0KbU6oDpS0SRmhseXNQODo0op2UtSBIiG0ZcMwsY1QBkggrHPnJIRHcACALUK",
-							openId);
+			String w1Token = (String) client1.get("WECHATSIGNATUREACCESSTOKEN");
+			String url = String.format(URLGET, w1Token, openId);
 			String response = Utilities.newHttpGet(url);
 			JSONObject resJ = JSON.parseObject(response);
 			System.out.println("response=" + response);
 			String errorcode = resJ.getString("errcode");
 			String unionId = resJ.getString("unionid");
-			if(errorcode != null ||unionId == null ) {
+			if (errorcode != null || unionId == null) {
 				type = 2;
-				String w2Token = "";
-				url = String
-						.format(URLGET,
-								"7_Set7zdglH3514RUpZ9zBLXsFs4KUgZQ33RMdmc7WceAK29EFRcaOZv1W781KYsgGk9rq8EB4f5UBePrg7Hkis3pMEKkMyvLysfVbhfCrSIs0OIcfZW86emDvI_UBMMeAIAEPP",
-								openId);
+				String w2Token = (String) client2.get("WECHATSIGNATUREACCESSTOKEN");
+				url = String.format(URLGET, w2Token, openId);
 				response = Utilities.newHttpGet(url);
 				resJ = JSON.parseObject(response);
 				System.out.println("response=" + response);
 				errorcode = resJ.getString("errcode");
 				unionId = resJ.getString("unionid");
-				if(errorcode != null || unionId == null) {
+				if (errorcode != null || unionId == null) {
 					continue;
 				}
 			}
-			
+
 			OPENID_UNIONID.put(openId, unionId + "#" + type);
 		}
 	}
